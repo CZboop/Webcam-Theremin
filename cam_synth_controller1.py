679a4b78960a7391a87905beaf6c229e0d7bc547
C Z <70140890+CZboop@users.noreply.github.com>
2021-10-26 22:25:32 +0100

    Basic hand sign detection

--- cam_synth_controller1.py
+++ cam_synth_controller1.py
@@ -20,13 +20,24 @@ while True:
     # checking for hand and if found:
     if results.multi_hand_landmarks:
         for hand_lms in results.multi_hand_landmarks:
+            # checking for hand sign one \m/
+            index_up_middle_down = hand_lms.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y<hand_lms.landmark[mp_hands.HandLandmark.INDEX_FINGER_DIP].y and hand_lms.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y>hand_lms.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_DIP].y
+            pinky_up_ring_down = hand_lms.landmark[mp_hands.HandLandmark.PINKY_TIP].y<hand_lms.landmark[mp_hands.HandLandmark.PINKY_DIP].y and hand_lms.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y>hand_lms.landmark[mp_hands.HandLandmark.RING_FINGER_DIP].y
+            spidey = False
+            if index_up_middle_down and pinky_up_ring_down:
+                spidey = True;
+
             # setting up OSC connection with default server supercollider listens on
             client = pyOSC3.OSCClient()
             client.connect( ( '127.0.0.1', 57120 ) )
             # creating and sending a OSC message with x and y coords of index fingertip, to the specified synth in the supercollider script
             msg = pyOSC3.OSCMessage()
             msg.setAddress("/theremin")
-            msg.extend([hand_lms.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x, hand_lms.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y])
+            # randomising the osc message in case of hand sign one
+            if spidey:
+                msg.extend([random.uniform(0,0.3), random.uniform(0,0.3), random.uniform(2,5)])
+            else:
+                msg.extend([hand_lms.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x, hand_lms.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y, 1])
             client.send(msg)
 
             # drawing hand landmarks, useful to check when it's detected
@@ -38,4 +49,6 @@ while True:
 
     # displaying video
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    # pressing q to quit window
+    if cv2.waitKey(1) == ord("q"):
+        break
